c242df10f970e49b22bdbfa0e00707a8618fd8cf
Eric Song <ericksong91@gmail.com>
2022-07-18 15:29:24 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply)
+
+let random = Math.floor(Math.random()*10) + 1;
+
+console.log(random);
+
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4;
+
+console.log(mod);
+
+let set = (1, 2, 3, 4, 20);
+let max = Math.max(set);
+
+console.log(max)
